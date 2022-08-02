4bd9b3ec6bcd9a31842dd24053d70cf1f7be34a0
Martín Gesualdo <mgesudev@gmail.com>
2022-08-01 22:42:04 -0300

    Cambios

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/e6a3fee8dd.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
-const initialState = { name: "", price: 0 }
+const initialProductState = { name: "", price: 0 }
+const initialMovementState = { type: "Compra", quantity: 0 }
 
 const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 
 export default function Home() {
-  const [product, setProduct] = useState(initialState)
+  const [selectedProductId, setSelectedProductId] = useState()
+  const [product, setProduct] = useState(initialProductState)
+  const [movement, setMovement] = useState(initialMovementState)
   const [products, setProducts] = useState([])
 
   const handleChange = (e) => {
@@ -15,8 +18,20 @@ export default function Home() {
       [inputName]: inputValue,
     })
   }
+  const handleMovementChange = (e) => {
+    const inputValue = e.target.value
+    setMovement({
+      ...movement,
+      quantity: +inputValue,
+    })
+  }
 
-  const handleClick = async (e) => {
+  const handleSelectType = (type) => {
+    console.log({ type })
+    setMovement({ ...movement, type })
+  }
+
+  const handleCreateProduct = async (e) => {
     e.preventDefault()
     try {
       const res = await fetch(`${baseUrl}/products`, {
@@ -27,11 +42,33 @@ export default function Home() {
         body: JSON.stringify(product),
       })
       const data = await res.json()
-      setProduct(initialState)
+      setProduct(initialProductState)
       console.log({ data })
       const newProducts = [data.product, ...products]
       setProducts(newProducts)
       // fetchProducts()
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+
+  const handleCreateMovement = async (e) => {
+    try {
+      const res = await fetch(
+        `${baseUrl}/products/movement/${selectedProductId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(movement),
+        }
+      )
+      const data = await res.json()
+      console.log({ data })
+      setProduct(initialMovementState)
+      setSelectedProductId(null)
+      fetchProducts()
     } catch (error) {
       console.log({ error })
     }
@@ -48,6 +85,8 @@ export default function Home() {
   useEffect(() => {
     fetchProducts()
   }, [])
+
+  console.log({ products })
 
   return (
     <>
@@ -71,19 +110,60 @@ export default function Home() {
               value={product.price}
               onChange={handleChange}
             />
-            <button onClick={handleClick} className="cursorp">
+            <button onClick={handleCreateProduct} className="cursorp">
               Crear producto
             </button>
           </form>
+          <h2 style={{ margin: "0.3rem" }}>
+            <u>Crear movimiento stock</u>
+          </h2>
+          <div className="df aic mb5">
+            {["Compra", "Venta"].map((type) => (
+              <div
+                onClick={() => handleSelectType(type)}
+                className="shadow mr5 p5 br5 cursorp"
+                key={type}
+                style={{
+                  backgroundColor:
+                    type === movement.type ? "lightblue" : "white",
+                }}
+              >
+                <span>{type}</span>
+              </div>
+            ))}
+          </div>
+          <input
+            type="number"
+            name="quantity"
+            className="onone"
+            value={movement.quantity}
+            defaultValue={movement.quantity}
+            onChange={handleMovementChange}
+          />
+          <button onClick={handleCreateMovement} className="cursorp">
+            Crear movimiento de stock
+          </button>
         </div>
         <div className="products-container">
-          {products.map(({ _id, name, price }) => (
-            <div className="product df aic jcsb p5 mb5 br5" key={_id}>
+          {products.map(({ _id, name, price, stock }) => (
+            <div
+              onClick={() => setSelectedProductId(_id)}
+              className="shadow df aic jcsb p5 mb5 br5"
+              style={{
+                backgroundColor:
+                  selectedProductId === _id ? "lightblue" : "white",
+                width: "100%",
+              }}
+              key={_id}
+            >
               <span>{name}</span>
-              <div className="df fdc">
-                <span>${price}</span>
-                <span
-                  style={{ color: "red", cursor: "pointer" }}
+              <div className="df aic">
+                <div className="df fdc mr5">
+                  <span>${price}</span>
+                  <span>Stock: {stock}</span>
+                </div>
+                <i
+                  className="fas fa-trash cursorp cred"
                   onClick={() => {
                     fetch(`${baseUrl}/products/${_id}`, { method: "DELETE" })
                       .then((res) => res.json())
@@ -91,9 +171,7 @@ export default function Home() {
                         console.log({ data })
                       })
                   }}
-                >
-                  BORRAR
-                </span>
+                />
               </div>
             </div>
           ))}
@@ -108,51 +186,8 @@ export default function Home() {
             margin: 0 auto;
           }
 
-          .onone {
-            outline: none;
-          }
-
-          .cursorp {
-            cursor: pointer;
-          }
-
-          .df {
-            display: flex;
-          }
-
-          .p5 {
-            padding: 0.5rem;
-          }
-
-          .br5 {
-            border-radius: 0.5rem;
-          }
-
-          .mb5 {
-            margin-bottom: 0.5rem;
-          }
-
-          .product {
+          .shadow {
             box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.1);
-          }
-          .aic {
-            align-items: center;
-          }
-
-          .jcc {
-            justify-content: center;
-          }
-
-          .jcsb {
-            justify-content: space-between;
-          }
-
-          .fdc {
-            flex-direction: column;
-          }
-
-          input {
-            margin-bottom: 0.5rem;
           }
 
           h1 {
@@ -164,6 +199,7 @@ export default function Home() {
             overflow-y: auto;
             max-height: 20rem;
             padding: 0.5rem;
+            width: 100%;
           }
 
           .container {
@@ -173,19 +209,6 @@ export default function Home() {
             margin-top: 5rem;
             border-radius: 0.5rem;
             padding: 1rem;
-          }
-
-          input {
-            padding: 0.5rem 0.75rem;
-            border: 1px solid lightgrey;
-            border-radius: 0.5rem;
-          }
-
-          button {
-            padding: 0.5rem 0.75rem;
-            border-radius: 0.5rem;
-            border: none;
-            background-color: rgba();
           }
         `}
       </style>
